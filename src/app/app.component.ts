80f3506b4722f2a7054f8d2d08f5715499bd6b4e
PanicaCC <panicacc@gmail.com>
2024-02-20 01:26:05 +0200

    lecture-1: init eslint

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import {Component} from '@angular/core'
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'Angular-learnjs-150224';
+  title = 'Angular-learnjs-150224'
 }
