a6af71d8a5c21e736368dac768021a522bb527bc
Dmitry <stan15091996@mail.ru>
2024-02-22 19:32:45 +0300

    Added prettier and lint rules

---
+++ .prettierrc.js
@@ -0,0 +1,12 @@
+const config = {
+  useTabs: false,
+  tabWidth: 4,
+  semi: true,
+  singleQuote: true,
+  printWidth: 130,
+  bracketSameLine: false,
+  arrowParens: "always",
+  bracketSpacing: true,
+};
+
+export default config;
