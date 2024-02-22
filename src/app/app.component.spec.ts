78874630e732b33022868b1899d5b2d5fbfd7d41
Letto228 <learnjs@MacBook-Pro-Egor.local>
2024-02-22 20:15:29 +0300

    fix eslint error

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -13,19 +13,23 @@ describe('AppComponent', () => {
     it('should create the app', () => {
         const fixture = TestBed.createComponent(AppComponent);
         const app = fixture.componentInstance;
+
         expect(app).toBeTruthy();
     });
 
     it(`should have as title 'Angular-learnjs-150224'`, () => {
         const fixture = TestBed.createComponent(AppComponent);
         const app = fixture.componentInstance;
+
         expect(app.title).toEqual('Angular-learnjs-150224');
     });
 
     it('should render title', () => {
         const fixture = TestBed.createComponent(AppComponent);
+
         fixture.detectChanges();
         const compiled = fixture.nativeElement as HTMLElement;
+
         expect(compiled.querySelector('.content span')?.textContent).toContain(
             'Angular-learnjs-150224 app is running!',
         );
