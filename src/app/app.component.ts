e309fb87b133b15a38142b1e22b5d2b551e68394
Letto228 <learnjs@MacBook-Pro-Egor.local>
2024-02-23 00:43:59 +0300

    feat: lecture 2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,16 @@
-import {Component} from '@angular/core';
+import {Component, ViewEncapsulation} from '@angular/core';
 
 @Component({
+    // selector: 'div#root',
+    // selector: 'button[myButton]', - <button myButton>...</button>
+    // selector: 'my-button', - <my-button><button>...</button></my-button>
     selector: 'app-root',
     templateUrl: './app.component.html',
     styleUrls: ['./app.component.css'],
+    // template: `<h1>Hello word</h1>`,
+    // styles: [''],
+    encapsulation: ViewEncapsulation.None,
+    interpolation: ['{{', '}}'],
 })
 export class AppComponent {
     title = 'Angular-learnjs-150224';
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,23 @@ import {BrowserModule} from '@angular/platform-browser';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {AppRoutingModule} from './app-routing.module';
 import {AppComponent} from './app.component';
+import {HeaderModule} from './components/header/header.module';
+
+// @NgModule({
+//     declarations: [HeaderComponent],
+//     exports: [HeaderComponent],
+// })
+// export class HeaderModule {}
+
+// declarations ~ const ...
+// exports ~ module.export = {...}
+// imports ~ import {...} from 'module';
 
 @NgModule({
-    declarations: [AppComponent],
-    imports: [BrowserModule, AppRoutingModule, BrowserAnimationsModule],
-    providers: [],
+    declarations: [AppComponent], // const ...
+    // exports: [AppComponent], // module.export = {...}
+    // imports: [HeaderModule], // import {...} from 'module';
+    imports: [BrowserModule, AppRoutingModule, BrowserAnimationsModule, HeaderModule],
     bootstrap: [AppComponent],
 })
 export class AppModule {}
