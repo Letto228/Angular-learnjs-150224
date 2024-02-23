01e6cb46c73e67e3f29058a71ec426c370490d27
Egor Levchenko <antytoto@gmail.com>
2024-02-23 16:24:54 +0300

    feat: created new card

--- src/app/pages/products-list/card/card/card.component.ts
+++ src/app/pages/products-list/card/card/card.component.ts
@@ -1,8 +1,17 @@
 import {Component} from '@angular/core';
+import {productMock} from '../../../../shared/products/product.mock';
 
 @Component({
     selector: 'app-card',
     templateUrl: './card.component.html',
     styleUrls: ['./card.component.css'],
 })
-export class CardComponent {}
+export class CardComponent {
+    cardName = productMock.name;
+    cardRating = productMock.rating;
+    cardFeedbacksCount = productMock.feedbacksCount;
+    cardPrice = productMock.price;
+    firstCardImage = productMock.images[0].url;
+    // eslint-disable-next-line no-console
+    console = () => console.log('YOU CLICKED ME');
+}
