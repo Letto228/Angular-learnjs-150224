5b155e7c1b42874e7a5d1c046d9138d1d886e524
asya2320 <asya.lestenkova2001@yandex.ru>
2024-02-24 17:46:49 +0300

    add data and functionality to card component

--- src/app/pages/products-list/card/card.component.ts
+++ src/app/pages/products-list/card/card.component.ts
@@ -1,8 +1,16 @@
 import {Component} from '@angular/core';
+import {productMock} from 'src/app/shared/products/product.mock';
+import {IProduct} from 'src/app/shared/products/product.interface';
 
 @Component({
     selector: 'app-card',
     templateUrl: './card.component.html',
     styleUrls: ['./card.component.css'],
 })
-export class CardComponent {}
+export class CardComponent {
+    product: IProduct = productMock;
+
+    getOrder() {
+        console.log('Order');
+    }
+}
--- src/app/pages/products-list/products-list.module.ts
+++ src/app/pages/products-list/products-list.module.ts
@@ -1,10 +1,13 @@
 import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
+import {MatButtonModule} from '@angular/material/button';
+import {MatCardModule} from '@angular/material/card';
 import {ProductsListComponent} from './products-list.component';
+import {CardComponent} from './card/card.component';
 
 @NgModule({
-    declarations: [ProductsListComponent],
-    imports: [CommonModule],
-    exports: [ProductsListComponent],
+    declarations: [ProductsListComponent, CardComponent],
+    imports: [CommonModule, MatButtonModule, MatCardModule],
+    exports: [ProductsListComponent, CardComponent],
 })
 export class ProductsListModule {}
