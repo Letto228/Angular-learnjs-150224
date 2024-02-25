0ccff55e96accf1777d1fc858619c7c003a9a0fa
Aleksei Maslov <maslovak@synergyteam.msk.ru>
2024-02-25 18:23:54 +0500

    feat: add product card component

---
+++ src/app/pages/products-list/card/card.component.ts
@@ -0,0 +1,19 @@
+import {Component} from '@angular/core';
+import {IProduct} from 'src/app/shared/products/product.interface';
+import {productMock} from '../../../shared/products/product.mock';
+
+@Component({
+    selector: 'app-card',
+    templateUrl: './card.component.html',
+    styleUrls: ['./card.component.css'],
+})
+export class CardComponent {
+    product: IProduct;
+    constructor() {
+        this.product = productMock;
+    }
+
+    clickCallback(event: MouseEvent): void {
+        event.stopPropagation();
+    }
+}
