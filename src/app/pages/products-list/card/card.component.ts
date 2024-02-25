d8e5341f35ec24b55489f5102073d692e381adda
Kateryna Artemieva <moruga.katya@gmail.com>
2024-02-25 16:25:39 +0000

    feat: add card from productMock

--- src/app/pages/products-list/card/card.component.ts
+++ src/app/pages/products-list/card/card.component.ts
@@ -1,8 +1,16 @@
 import {Component} from '@angular/core';
+import {productMock} from 'src/app/shared/products/product.mock';
 
 @Component({
     selector: 'app-card',
     templateUrl: './card.component.html',
     styleUrls: ['./card.component.css'],
 })
-export class CardComponent {}
+export class CardComponent {
+    card = productMock;
+
+    buy(): void {
+        // eslint-disable-next-line no-console
+        console.log('Buy product');
+    }
+}
