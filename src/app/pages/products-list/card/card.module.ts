a6238df8daa4989643f5adc41e3fda0ff17442fa
Ivan Glushkov <gis.glushkov@yandex.ru>
2024-02-25 11:36:37 +0300

    feat: card module was added

---
+++ src/app/pages/products-list/card/card.module.ts
@@ -0,0 +1,12 @@
+import {NgModule} from '@angular/core';
+import {CommonModule} from '@angular/common';
+import {MatCardModule} from '@angular/material/card';
+import {MatButtonModule} from '@angular/material/button';
+import {CardComponent} from './card.component';
+
+@NgModule({
+    declarations: [CardComponent],
+    imports: [CommonModule, MatCardModule, MatButtonModule],
+    exports: [CardComponent],
+})
+export class CardModule {}
--- src/app/pages/products-list/products-list.module.ts
+++ src/app/pages/products-list/products-list.module.ts
@@ -1,14 +1,11 @@
 import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
-import {MatCardModule} from '@angular/material/card';
-import {MatGridListModule} from '@angular/material/grid-list';
-import {MatButtonModule} from '@angular/material/button';
-import {CardComponent} from './card/card.component';
 import {ProductsListComponent} from './products-list.component';
+import {CardModule} from './card/card.module';
 
 @NgModule({
-    declarations: [ProductsListComponent, CardComponent],
-    imports: [CommonModule, MatCardModule, MatGridListModule, MatButtonModule],
+    declarations: [ProductsListComponent],
+    imports: [CommonModule, CardModule],
     exports: [ProductsListComponent],
 })
 export class ProductsListModule {}
