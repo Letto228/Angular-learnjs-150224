9449e64de405a995c6b4e31cf8a7f136ad03f64b
derguzov <derguzov.nv@gazprom-neft.ru>
2024-02-26 16:33:19 +0300

    feat: added card module

--- src/app/pages/products-list/card/card.module.ts
+++ src/app/pages/products-list/card/card.module.ts
@@ -1,8 +1,12 @@
 import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
+import {MatCardModule} from '@angular/material/card';
+import {MatButtonModule} from '@angular/material/button';
+import {CardComponent} from './card.component';
 
 @NgModule({
-    declarations: [],
-    imports: [CommonModule],
+    declarations: [CardComponent],
+    imports: [CommonModule, MatCardModule, MatButtonModule],
+    exports: [CardComponent],
 })
 export class CardModule {}
--- src/app/pages/products-list/products-list.module.ts
+++ src/app/pages/products-list/products-list.module.ts
@@ -1,13 +1,11 @@
 import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
-import {MatCardModule} from '@angular/material/card';
-import {MatButtonModule} from '@angular/material/button';
 import {ProductsListComponent} from './products-list.component';
-import {CardComponent} from './card/card.component';
+import {CardModule} from './card/card.module';
 
 @NgModule({
-    declarations: [ProductsListComponent, CardComponent],
-    imports: [CommonModule, MatCardModule, MatButtonModule],
+    declarations: [ProductsListComponent],
+    imports: [CommonModule, CardModule],
     exports: [ProductsListComponent],
 })
 export class ProductsListModule {}
