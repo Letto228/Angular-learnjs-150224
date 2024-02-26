3d806cc8e6a38b5edb979a977e9771576083b6f3
Angelina1Blokhina <blohina02@icloud.com>
2024-02-26 13:50:40 +0300

    feat: add card

---
+++ src/app/pages/products-list/card/card.component.ts
@@ -0,0 +1,13 @@
+import {Component} from '@angular/core';
+import {IProduct} from 'src/app/shared/products/product.interface';
+import {productMock} from 'src/app/shared/products/product.mock';
+
+@Component({
+    selector: 'app-card',
+    templateUrl: './card.component.html',
+    styleUrls: ['./card.component.css'],
+})
+export class CardComponent {
+    plane: IProduct = productMock;
+    clickCallback() {}
+}
