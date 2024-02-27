176ef7e6882f86e8ab57c7ae9757eac781abc74e
Andrey Poymanov <indebrigello36@gmail.com>
2024-02-28 01:53:40 +0300

    feat: add product-card

---
+++ src/app/pages/products-list/card/card.component.ts
@@ -0,0 +1,20 @@
+import {Component} from '@angular/core';
+import {IProduct} from 'src/app/shared/products/product.interface';
+import {productMock} from 'src/app/shared/products/product.mock';
+
+@Component({
+    selector: 'app-card',
+    templateUrl: './card.component.html',
+    styleUrls: ['./card.component.css'],
+})
+export class CardComponent {
+    product: IProduct;
+
+    constructor() {
+        this.product = productMock;
+    }
+
+    clickBuyButton() {
+        alert('buy clicked');
+    }
+}
