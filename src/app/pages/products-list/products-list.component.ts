bfbc90e27c9a68cf5379955140dcc1d114d0574b
derguzov <derguzov.nv@gazprom-neft.ru>
2024-02-27 14:51:52 +0300

    feat: added input and output for card

--- src/app/pages/products-list/products-list.component.ts
+++ src/app/pages/products-list/products-list.component.ts
@@ -1,4 +1,5 @@
 import {Component} from '@angular/core';
+import {productsMock} from '../../shared/products/products.mock';
 
 @Component({
     selector: 'app-products-list',
@@ -6,6 +7,20 @@ import {Component} from '@angular/core';
     styleUrls: ['./products-list.component.css'],
 })
 export class ProductsListComponent {
+    isProductBought = false;
+
+    changeProductStatus() {
+        this.isProductBought = !this.isProductBought;
+        // eslint-disable-next-line no-console
+        console.log(this.isProductBought);
+    }
+
+    readonly product = productsMock[0];
+
+    buyProduct() {
+        this.changeProductStatus();
+    }
+
     onCardClick() {
         // eslint-disable-next-line no-console
         console.log('Card click');
