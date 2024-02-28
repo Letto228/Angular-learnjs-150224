ff7831e60cbde1503711820b6a4b15cbd3d7d147
a.semenets <a.semenets@idaproject.com>
2024-02-28 10:05:55 +0300

    feat: card complete

--- src/app/pages/products-list/card/card.component.ts
+++ src/app/pages/products-list/card/card.component.ts
@@ -1,8 +1,20 @@
 import {Component} from '@angular/core';
+import {productMock} from '../../../shared/products/product.mock';
 
 @Component({
     selector: 'app-card',
     templateUrl: './card.component.html',
     styleUrls: ['./card.component.css'],
 })
-export class CardComponent {}
+export class CardComponent {
+    readonly product = productMock;
+
+    onCardClick(): void {
+        // console.log('on card click!');
+    }
+
+    onBuyClick(event: MouseEvent): void {
+        event.stopPropagation();
+        // console.log('on buy button click!');
+    }
+}
