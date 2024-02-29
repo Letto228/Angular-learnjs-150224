cf5603d8c4c4de4add262dd39604ae2d26ad10f1
Dmitry <stan15091996@mail.ru>
2024-02-29 17:36:09 +0300

    feat: added output-input comunication for card component

--- src/app/pages/products-list/products-list.component.ts
+++ src/app/pages/products-list/products-list.component.ts
@@ -1,4 +1,6 @@
 import {Component} from '@angular/core';
+import {Product} from 'src/app/shared/products/product.interface';
+import {productsMock} from 'src/app/shared/products/products.mock';
 
 @Component({
     selector: 'app-products-list',
@@ -6,6 +8,17 @@ import {Component} from '@angular/core';
     styleUrls: ['./products-list.component.css'],
 })
 export class ProductsListComponent {
+    readonly products = productsMock;
+    readonly productsCart: Product[] = [];
+
+    addInCart(event: string): void {
+        const product = this.products.find(product => product._id === event);
+
+        if (product) {
+            this.productsCart.push(product);
+        }
+    }
+
     onCardClick() {
         // eslint-disable-next-line no-console
         console.log('Card click');
