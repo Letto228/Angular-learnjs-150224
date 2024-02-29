340e1f07a16dde061ae4042c6ed44b24a3be0de7
Letto228 <learnjs@MacBook-Pro-Egor.local>
2024-02-29 19:16:22 +0300

    feat: lecture 3 homework

--- src/app/pages/products-list/card/card.component.ts
+++ src/app/pages/products-list/card/card.component.ts
@@ -1,5 +1,5 @@
-import {Component} from '@angular/core';
-import {productsMock} from '../../../shared/products/products.mock';
+import {Component, EventEmitter, Input, Output} from '@angular/core';
+import {Product} from '../../../shared/products/product.interface';
 
 // const user: User = {
 
@@ -11,7 +11,7 @@ import {productsMock} from '../../../shared/products/products.mock';
     styleUrls: ['./card.component.css'],
 })
 export class CardComponent {
-    readonly product = productsMock[0];
+    // readonly product = productsMock[0];
     // user: {name: string} = {name: 'Alex'};
     // user = user;
 
@@ -19,14 +19,24 @@ export class CardComponent {
     //     this.user = null;
     // }
 
+    @Input() product: Product | null = null;
+
+    @Output() readonly buy = new EventEmitter<Product['_id']>();
+
     onProductBuy(event: Event) {
         event.stopPropagation();
 
         // eslint-disable-next-line no-console
-        console.log('Buy product');
+        // console.log('Buy product');
+
+        if (!this.product) {
+            return;
+        }
+
+        this.buy.emit(this.product._id);
     }
 
     isStarActive(starIndex: number): boolean {
-        return this.product.rating >= starIndex;
+        return !!this.product && this.product.rating >= starIndex;
     }
 }
