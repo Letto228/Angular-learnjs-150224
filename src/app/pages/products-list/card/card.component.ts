816b12f069c0b4a0c7839432c9096240d573c64e
Aleksei Maslov <maslovak@synergyteam.msk.ru>
2024-02-29 22:36:40 +0500

    feat: add input/output for product

--- src/app/pages/products-list/card/card.component.ts
+++ src/app/pages/products-list/card/card.component.ts
@@ -1,9 +1,5 @@
-import {Component} from '@angular/core';
-import {productsMock} from '../../../shared/products/products.mock';
-
-// const user: User = {
-
-// }
+import {Component, EventEmitter, Input, Output} from '@angular/core';
+import {Product} from '../../../shared/products/product.interface';
 
 @Component({
     selector: 'app-card',
@@ -11,22 +7,15 @@ import {productsMock} from '../../../shared/products/products.mock';
     styleUrls: ['./card.component.css'],
 })
 export class CardComponent {
-    readonly product = productsMock[0];
-    // user: {name: string} = {name: 'Alex'};
-    // user = user;
-
-    // constructor() {
-    //     this.user = null;
-    // }
+    @Input() product: Product | null = null;
+    @Output() productBuy = new EventEmitter<string>();
 
     onProductBuy(event: Event) {
         event.stopPropagation();
-
-        // eslint-disable-next-line no-console
-        console.log('Buy product');
+        this.productBuy.emit(this.product?._id);
     }
 
     isStarActive(starIndex: number): boolean {
-        return this.product.rating >= starIndex;
+        return this.product ? this.product.rating >= starIndex : false;
     }
 }
--- src/app/pages/products-list/products-list.component.ts
+++ src/app/pages/products-list/products-list.component.ts
@@ -1,4 +1,5 @@
 import {Component} from '@angular/core';
+import {productsMock} from '../../shared/products/products.mock';
 
 @Component({
     selector: 'app-products-list',
@@ -6,8 +7,14 @@ import {Component} from '@angular/core';
     styleUrls: ['./products-list.component.css'],
 })
 export class ProductsListComponent {
+    readonly products = productsMock;
     onCardClick() {
         // eslint-disable-next-line no-console
         console.log('Card click');
+    }
+
+    onProductBuy(event: string) {
+        // eslint-disable-next-line no-console
+        console.log('Buy product', event);
     }
 }
