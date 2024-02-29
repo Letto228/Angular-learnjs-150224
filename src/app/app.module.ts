66ed6a9c255fb5e8fd2397fa941b71c5bb8c17d1
Letto228 <learnjs@MacBook-Pro-Egor.local>
2024-02-29 22:51:58 +0300

    feat: lecture 4

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import {NgModule} from '@angular/core';
 import {BrowserModule} from '@angular/platform-browser';
 
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {MatListModule} from '@angular/material/list';
 import {AppRoutingModule} from './app-routing.module';
 import {AppComponent} from './app.component';
 import {HeaderModule} from './components/header/header.module';
@@ -17,6 +18,7 @@ import {SidenavModule} from './components/sidenav/sidenav.module';
         HeaderModule,
         ProductsListModule,
         SidenavModule,
+        MatListModule,
     ],
     bootstrap: [AppComponent],
 })
--- src/app/components/sidenav/sidenav.component.ts
+++ src/app/components/sidenav/sidenav.component.ts
@@ -1,16 +1,94 @@
-import {Component, EventEmitter, Input, Output} from '@angular/core';
+import {
+    Component,
+    ContentChild,
+    ElementRef,
+    EventEmitter,
+    Input,
+    OnInit,
+    Output,
+    TemplateRef,
+    ViewChild,
+    ViewContainerRef,
+} from '@angular/core';
+import {MatDrawer} from '@angular/material/sidenav';
 
 @Component({
     selector: 'app-sidenav',
     templateUrl: './sidenav.component.html',
     styleUrls: ['./sidenav.component.css'],
 })
-export class SidenavComponent {
+export class SidenavComponent implements OnInit {
     @Input() isSidenavOpened = false;
+    // @Input() navigationTemplate: TemplateRef<unknown> | null = null;
+    // @Input() set navigationTemplate(template: TemplateRef<unknown>) {
+    //     this.viewportContainer?.clear();
+    //     this.viewportContainer?.createEmbeddedView(template);
+    //     // const view = this.viewportContainer?.createEmbeddedView(template);
+
+    //     // console.log(view?.destroy());
+
+    //     // this.viewportContainer?.clear();
+
+    //     // this.viewportContainer?.createEmbeddedView(template);
+    //     // this.viewportContainer?.createEmbeddedView(template);
+    //     // setInterval(() => {
+    //     //     this.viewportContainer?.createEmbeddedView(template);
+    //     // }, 2000);
+    // }
 
     @Output() readonly isSidenavOpenedChange = new EventEmitter<boolean>();
 
-    toggleIsSidenavOpened() {
-        this.isSidenavOpenedChange.emit(!this.isSidenavOpened);
+    ngOnInit(): void {
+        this.insertNavigation();
+    }
+
+    /* eslint-disable @typescript-eslint/prefer-readonly */
+    // @ViewChild('drawer')
+    @ViewChild(MatDrawer)
+    private drawer: MatDrawer | undefined;
+
+    // @ViewChild(MatDrawer, {read: ElementRef})
+    // private set drawerElement(elementRef: ElementRef | undefined) {
+    //     console.log(elementRef?.nativeElement);
+    // };
+
+    // @ViewChild(MatDrawer, {read: ViewContainerRef})
+    // private drawerViewContainer: ViewContainerRef | undefined;
+
+    @ViewChild('viewport', {read: ViewContainerRef, static: true})
+    private viewportContainer: ViewContainerRef | undefined;
+
+    @ContentChild('p', {read: ElementRef, descendants: false})
+    private set checkContent(elementRef: ElementRef | undefined) {
+        // eslint-disable-next-line no-console
+        console.log(elementRef);
+    }
+
+    @ContentChild('navigationTemplate', {static: true})
+    private navigationTemplate: TemplateRef<unknown> | undefined;
+    /* eslint-enable @typescript-eslint/prefer-readonly */
+
+    // constructor() {
+    //     console.log(this.drawer);
+    // }
+
+    // toggleIsSidenavOpened() {
+    //     this.isSidenavOpenedChange.emit(!this.isSidenavOpened);
+    // }
+
+    toggle() {
+        // drawer.toggle()
+
+        // console.log(this.drawer, this.drawerElement, this.drawerViewContainer);
+
+        this.drawer?.toggle();
+    }
+
+    private insertNavigation() {
+        if (!this.navigationTemplate) {
+            return;
+        }
+
+        this.viewportContainer?.createEmbeddedView(this.navigationTemplate);
     }
 }
