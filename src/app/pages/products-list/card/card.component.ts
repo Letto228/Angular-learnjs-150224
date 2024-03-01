cf922c65ece86caa8b2a4a9887a462c0f2af0705
Anna Ivchenko <anna.ivchenko@softgamings.com>
2024-03-01 03:25:29 +0300

    feat: products in list(lecture3)

--- src/app/pages/products-list/card/card.component.ts
+++ src/app/pages/products-list/card/card.component.ts
@@ -1,5 +1,5 @@
-import {Component} from '@angular/core';
-import {productsMock} from '../../../shared/products/products.mock';
+import {Component, EventEmitter, Input, Output} from '@angular/core';
+import {Product} from '../../../shared/products/product.interface';
 
 // const user: User = {
 
@@ -11,7 +11,8 @@ import {productsMock} from '../../../shared/products/products.mock';
     styleUrls: ['./card.component.css'],
 })
 export class CardComponent {
-    readonly product = productsMock[0];
+    @Input() product: Product | null = null;
+    @Output() addToCart = new EventEmitter<Product['_id'] | undefined>();
     // user: {name: string} = {name: 'Alex'};
     // user = user;
 
@@ -21,12 +22,13 @@ export class CardComponent {
 
     onProductBuy(event: Event) {
         event.stopPropagation();
+        this.addToCart.emit(this.product?._id);
 
         // eslint-disable-next-line no-console
         console.log('Buy product');
     }
 
     isStarActive(starIndex: number): boolean {
-        return this.product.rating >= starIndex;
+        return this.product ? this.product.rating >= starIndex : false;
     }
 }
--- src/app/pages/products-list/products-list.component.ts
+++ src/app/pages/products-list/products-list.component.ts
@@ -1,4 +1,6 @@
 import {Component} from '@angular/core';
+import {Product} from '../../shared/products/product.interface';
+import {productsMock} from '../../shared/products/products.mock';
 
 @Component({
     selector: 'app-products-list',
@@ -9,5 +11,18 @@ export class ProductsListComponent {
     onCardClick() {
         // eslint-disable-next-line no-console
         console.log('Card click');
+    }
+
+    addToCart(id: Product['_id'] | undefined) {
+        // eslint-disable-next-line no-console
+        console.log(id);
+    }
+
+    // вообще я не планировала эту аццкую покупалку (что оно по клику все будет обновляться), но по приколу оставлю, добавил в корзину товар - ну и нечего на него больше смотреть, вот тебе другой :D
+    chooseRandomProduct(): Product {
+        const randomProduct: number =
+            Math.floor(Math.random() * (productsMock.length - 1 - 0 + 1)) + 0;
+
+        return productsMock[randomProduct];
     }
 }
