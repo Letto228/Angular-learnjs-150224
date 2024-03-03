0f6572e4b9ec51e9e8398059d13943d2110530fe
Alexandr Kudryavtcev <k.v.alexandr@gmail.com>
2024-03-03 18:57:44 +0400

    chore: clear popup module

--- src/app/components/popup-host/popup-host.module.ts
+++ src/app/components/popup-host/popup-host.module.ts
@@ -1,10 +1,8 @@
 import {NgModule} from '@angular/core';
 import {PopupHostComponent} from './popup-host.component';
-import {MatDialogModule} from '@angular/material/dialog';
 
 @NgModule({
     declarations: [PopupHostComponent],
-    imports: [MatDialogModule],
     exports: [PopupHostComponent],
 })
 export class PopupHostModule {}
