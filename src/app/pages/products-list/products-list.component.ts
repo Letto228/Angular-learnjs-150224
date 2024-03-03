ef9784ce3c77df7fa8f653164e36a60b3e91eb8a
Andrey Poymanov <indebrigello36@gmail.com>
2024-03-04 01:40:43 +0300

    feat: hw lecture-3

--- src/app/pages/products-list/products-list.component.ts
+++ src/app/pages/products-list/products-list.component.ts
@@ -1,4 +1,6 @@
 import {Component} from '@angular/core';
+import {Product} from 'src/app/shared/products/product.interface';
+import {productsMock} from 'src/app/shared/products/products.mock';
 
 @Component({
     selector: 'app-products-list',
@@ -6,8 +8,20 @@ import {Component} from '@angular/core';
     styleUrls: ['./products-list.component.css'],
 })
 export class ProductsListComponent {
+    readonly products = productsMock;
+
     onCardClick() {
         // eslint-disable-next-line no-console
         console.log('Card click');
+    }
+
+    addToBasket(event: Product['_id'] | undefined) {
+        // eslint-disable-next-line no-console
+        console.log('Buy', event);
+    }
+
+    getProduct(): Product {
+        // return this.products[Math.floor(Math.random() * (8 - 1 + 1) + 1)];
+        return this.products[2];
     }
 }
