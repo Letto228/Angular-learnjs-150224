fa36992acb01f81d9805f21e00798e3563f55748
Деникаев Эмиль Рамилевич <e.denikaev@gulliver-ul.local>
2024-03-03 06:21:42 +0400

    feat: add card component

---
+++ src/app/pages/products-list/card/card.component.ts
@@ -0,0 +1,16 @@
+import {Component} from '@angular/core';
+import {IProduct} from 'src/app/shared/products/product.interface';
+import {productMock} from 'src/app/shared/products/product.mock';
+
+@Component({
+    selector: 'app-card',
+    templateUrl: './card.component.html',
+    styleUrls: ['./card.component.css'],
+})
+export class CardComponent {
+    readonly product: IProduct = productMock;
+
+    onProductBuy(event: Event) {
+        event.stopPropagation();
+    }
+}
