774a252eab987945c4b6021fd2b0dc508d6a4517
RuslanSalimov <mail-spam2012@yandex.ru>
2024-03-05 02:12:28 +0500

    feat: add dynamic product component

--- src/app/pages/products-list/card/card.component.ts
+++ src/app/pages/products-list/card/card.component.ts
@@ -1,9 +1,5 @@
-import {Component} from '@angular/core';
-import {productsMock} from '../../../shared/products/products.mock';
-
-// const user: User = {
-
-// }
+import {Component, EventEmitter, Input, Output} from '@angular/core';
+import {Product} from '../../../shared/products/product.interface';
 
 @Component({
     selector: 'app-card',
@@ -11,22 +7,18 @@ import {productsMock} from '../../../shared/products/products.mock';
     styleUrls: ['./card.component.css'],
 })
 export class CardComponent {
-    readonly product = productsMock[0];
-    // user: {name: string} = {name: 'Alex'};
-    // user = user;
-
-    // constructor() {
-    //     this.user = null;
-    // }
+    @Input() product: Product | null = null;
+    @Output() readonly buyProduct = new EventEmitter<string>();
 
     onProductBuy(event: Event) {
         event.stopPropagation();
 
-        // eslint-disable-next-line no-console
-        console.log('Buy product');
+        if (this.product) {
+            this.buyProduct.emit(this.product._id);
+        }
     }
 
     isStarActive(starIndex: number): boolean {
-        return this.product.rating >= starIndex;
+        return !!this.product && this.product.rating >= starIndex;
     }
 }
