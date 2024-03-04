678d9720ce1e72944c45452a55dfd948c0ee183c
Letto228 <learnjs@MacBook-Pro-Egor.local>
2024-03-04 17:51:58 +0300

    chore: pre lecture 5

--- src/app/components/sidenav/sidenav.component.ts
+++ src/app/components/sidenav/sidenav.component.ts
@@ -14,18 +14,17 @@ import {MatDrawer} from '@angular/material/sidenav';
     styleUrls: ['./sidenav.component.css'],
 })
 export class SidenavComponent implements OnInit {
-    /* eslint-disable @typescript-eslint/prefer-readonly */
-
+    // eslint-disable-next-line @typescript-eslint/prefer-readonly
     @ViewChild(MatDrawer)
     private drawer: MatDrawer | undefined;
 
+    // eslint-disable-next-line @typescript-eslint/prefer-readonly
     @ViewChild('viewport', {read: ViewContainerRef, static: true})
     private viewportContainer: ViewContainerRef | undefined;
 
+    // eslint-disable-next-line @typescript-eslint/prefer-readonly
     @ContentChild('navigationTemplate', {static: true})
     private navigationTemplate: TemplateRef<unknown> | undefined;
-
-    /* eslint-enable @typescript-eslint/prefer-readonly */
 
     ngOnInit(): void {
         this.insertNavigation();
