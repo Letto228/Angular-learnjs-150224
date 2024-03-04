984514dcfc5ff9f4f374b93f91d6d880e6e585fa
Angelina1Blokhina <blohina02@icloud.com>
2024-03-04 10:45:10 +0300

    feat: input and output

--- src/app/pages/products-list/card/card.component.ts
+++ src/app/pages/products-list/card/card.component.ts
@@ -1,5 +1,5 @@
-import {Component} from '@angular/core';
-import {productsMock} from '../../../shared/products/products.mock';
+import {Component, Input, Output, EventEmitter} from '@angular/core';
+import {Product} from 'src/app/shared/products/product.interface';
 
 // const user: User = {
 
@@ -11,22 +11,25 @@ import {productsMock} from '../../../shared/products/products.mock';
     styleUrls: ['./card.component.css'],
 })
 export class CardComponent {
-    readonly product = productsMock[0];
-    // user: {name: string} = {name: 'Alex'};
-    // user = user;
+    @Input() product!: Product | null;
+    @Output() buyProduct = new EventEmitter<Product>();
 
-    // constructor() {
-    //     this.user = null;
-    // }
+    // readonly product = productsMock[0];
 
     onProductBuy(event: Event) {
         event.stopPropagation();
+
+        if (this.product) {
+            this.buyProduct.emit(this.product);
+        }
 
         // eslint-disable-next-line no-console
         console.log('Buy product');
     }
 
     isStarActive(starIndex: number): boolean {
-        return this.product.rating >= starIndex;
+        return (
+            this.product !== null && this.product !== undefined && this.product.rating >= starIndex
+        );
     }
 }
--- src/app/pages/products-list/products-list.component.ts
+++ src/app/pages/products-list/products-list.component.ts
@@ -1,4 +1,6 @@
 import {Component} from '@angular/core';
+import {Product} from 'src/app/shared/products/product.interface';
+import {productsMock} from 'src/app/shared/products/products.mock';
 
 @Component({
     selector: 'app-products-list',
@@ -6,6 +8,13 @@ import {Component} from '@angular/core';
     styleUrls: ['./products-list.component.css'],
 })
 export class ProductsListComponent {
+    readonly productListMock = productsMock;
+
+    onBuy(product: Product) {
+        // eslint-disable-next-line no-console
+        console.log('Товар купили:', product.name);
+    }
+
     onCardClick() {
         // eslint-disable-next-line no-console
         console.log('Card click');
