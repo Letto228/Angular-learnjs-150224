6f35b8d769b81d9414250867d15fc80872f705e4
Viieyeon <98655811+Viieyeon@users.noreply.github.com>
2024-03-07 14:58:45 +0300

    chore: method onProductBuy

--- src/app/pages/products-list/card/card.component.ts
+++ src/app/pages/products-list/card/card.component.ts
@@ -1,5 +1,5 @@
-import {Component} from '@angular/core';
-import {productsMock} from '../../../shared/products/products.mock';
+import {Component, EventEmitter, Input, Output} from '@angular/core';
+import {Product} from 'src/app/shared/products/product.interface';
 
 // const user: User = {
 
@@ -11,7 +11,7 @@ import {productsMock} from '../../../shared/products/products.mock';
     styleUrls: ['./card.component.css'],
 })
 export class CardComponent {
-    readonly product = productsMock[0];
+    // readonly product = productsMock[0];
     // user: {name: string} = {name: 'Alex'};
     // user = user;
 
@@ -19,14 +19,22 @@ export class CardComponent {
     //     this.user = null;
     // }
 
+    @Input() product: Product | null = null;
+
+    @Output() bayProduct = new EventEmitter<Product['_id']>();
+
     onProductBuy(event: Event) {
         event.stopPropagation();
+
+        if (this.product?._id) {
+            this.bayProduct.emit(this.product._id);
+        }
 
         // eslint-disable-next-line no-console
         console.log('Buy product');
     }
 
     isStarActive(starIndex: number): boolean {
-        return this.product.rating >= starIndex;
+        return !!this.product && this.product.rating >= starIndex;
     }
 }
--- src/app/pages/products-list/products-list.component.ts
+++ src/app/pages/products-list/products-list.component.ts
@@ -1,4 +1,6 @@
 import {Component} from '@angular/core';
+import {Product} from 'src/app/shared/products/product.interface';
+import {productsMock} from 'src/app/shared/products/products.mock';
 
 @Component({
     selector: 'app-products-list',
@@ -6,8 +8,15 @@ import {Component} from '@angular/core';
     styleUrls: ['./products-list.component.css'],
 })
 export class ProductsListComponent {
-    onCardClick() {
+    readonly productParent = productsMock;
+
+    bayProductItem(id: Product['_id']) {
         // eslint-disable-next-line no-console
-        console.log('Card click');
+        console.log(`Buy product${id}`);
     }
+
+    // onCardClick() {
+    //     // eslint-disable-next-line no-console
+    //     console.log('Card click');
+    // }
 }
