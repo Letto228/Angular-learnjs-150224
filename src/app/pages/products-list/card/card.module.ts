97727044192c1226347d6ee4c29313a2541bafe1
Vladimir Smirnov <vsmirnov@lanit.ru>
2024-03-08 22:19:21 +0500

    feat: data exchange with product card

--- src/app/pages/products-list/card/card.module.ts
+++ src/app/pages/products-list/card/card.module.ts
@@ -1,5 +1,5 @@
 import {NgModule} from '@angular/core';
-import {CommonModule, NgOptimizedImage} from '@angular/common';
+import {CommonModule} from '@angular/common';
 import {MatIconModule} from '@angular/material/icon';
 import {MatButtonModule} from '@angular/material/button';
 import {MatCardModule} from '@angular/material/card';
@@ -7,7 +7,7 @@ import {CardComponent} from './card.component';
 
 @NgModule({
     declarations: [CardComponent],
-    imports: [CommonModule, MatCardModule, MatButtonModule, MatIconModule, NgOptimizedImage],
+    imports: [CommonModule, MatCardModule, MatButtonModule, MatIconModule],
     exports: [CardComponent],
 })
 export class CardModule {}
