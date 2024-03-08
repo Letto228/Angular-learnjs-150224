0c594c73fc1d89701724d8ad4289588ec40849ca
Ivan Glushkov <gis.glushkov@yandex.ru>
2024-03-08 12:55:32 +0300

    feat: homework 4

---
+++ src/app/components/popup-host/popup-host.component.ts
@@ -0,0 +1,27 @@
+import {Component, Input, TemplateRef, ViewChild, ViewContainerRef} from '@angular/core';
+
+@Component({
+    selector: 'app-popup-host',
+    templateUrl: './popup-host.component.html',
+    styleUrls: ['./popup-host.component.css'],
+})
+export class PopupHostComponent {
+    get isTemplateEmpty(): boolean {
+        return !this.viewportContainer?.length;
+    }
+
+    @Input() set template(templateRef: TemplateRef<unknown> | null) {
+        this.updateTemplate(templateRef);
+    }
+
+    @ViewChild('viewport', {read: ViewContainerRef})
+    private readonly viewportContainer: ViewContainerRef | undefined;
+
+    private updateTemplate(templateRef: TemplateRef<unknown> | null): void {
+        this.viewportContainer?.clear();
+
+        if (templateRef) {
+            this.viewportContainer?.createEmbeddedView(templateRef);
+        }
+    }
+}
