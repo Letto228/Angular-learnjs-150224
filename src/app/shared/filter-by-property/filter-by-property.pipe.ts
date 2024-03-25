8d6d69dc1537bf25d7e7bdda343a5cc52fc76572
Dmitry <stan15091996@mail.ru>
2024-03-25 17:54:02 +0300

    feat: added filter pipe

--- src/app/shared/filter-by-property/filter-by-property.pipe.ts
+++ src/app/shared/filter-by-property/filter-by-property.pipe.ts
@@ -4,11 +4,7 @@ import {Pipe, PipeTransform} from '@angular/core';
     name: 'filterByProperty',
 })
 export class FilterByPropertyPipe implements PipeTransform {
-    transform<T, N extends keyof T>(
-        value: T[],
-        propertyName: N,
-        searchPropertyValu: T[N],
-    ): T[] | [] {
+    transform<T, N extends keyof T>(value: T[], propertyName: N, searchPropertyValu: T[N]): T[] {
         if (typeof searchPropertyValu === 'string') {
             return value.filter(item => {
                 const itemString = item[propertyName] as string;
