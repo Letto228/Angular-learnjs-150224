f29b1cfbcda8c29fe4d64487c4aa06fbcef3825a
Letto228 <learnjs@MacBook-Pro-Egor.local>
2024-03-26 11:07:09 +0300

    feat: add logo navigation for lecture 10 homework

--- src/app/components/header/header.module.ts
+++ src/app/components/header/header.module.ts
@@ -3,11 +3,12 @@ import {CommonModule} from '@angular/common';
 import {MatToolbarModule} from '@angular/material/toolbar';
 import {MatIconModule} from '@angular/material/icon';
 import {MatButtonModule} from '@angular/material/button';
+import {RouterModule} from '@angular/router';
 import {HeaderComponent} from './header.component';
 
 @NgModule({
     declarations: [HeaderComponent],
-    imports: [CommonModule, MatToolbarModule, MatIconModule, MatButtonModule],
+    imports: [CommonModule, MatToolbarModule, MatIconModule, MatButtonModule, RouterModule],
     exports: [HeaderComponent],
 })
 export class HeaderModule {}
