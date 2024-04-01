ffda900232c169e6ddaa763869c19f39100dcc9e
Анастасия Лестенькова <asya.lestenkova2001@yandex.ru>
2024-04-01 13:01:14 +0300

    feat: add navigation

--- src/app/components/sidenav/categories-select/categories-select.module.ts
+++ src/app/components/sidenav/categories-select/categories-select.module.ts
@@ -2,11 +2,12 @@ import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {MatButtonModule} from '@angular/material/button';
 import {MatExpansionModule} from '@angular/material/expansion';
+import {RouterLink} from '@angular/router';
 import {CategoriesSelectComponent} from './categories-select.component';
 
 @NgModule({
     declarations: [CategoriesSelectComponent],
-    imports: [CommonModule, MatButtonModule, MatExpansionModule],
+    imports: [CommonModule, MatButtonModule, MatExpansionModule, RouterLink],
     exports: [CategoriesSelectComponent],
 })
 export class CategoriesSelectModule {}
