d1173c17ecc06aee4c36f9941e1a2ffdc8f6cb5b
annchapon <annchapon@gmail.com>
2024-04-27 11:42:44 +0300

    feat: add categories navigation

--- src/app/pages/products-list/products-list-routing.module.ts
+++ src/app/pages/products-list/products-list-routing.module.ts
@@ -7,6 +7,10 @@ const routes: Routes = [
         path: '',
         component: ProductsListComponent,
     },
+    {
+        path: ':categoryId',
+        component: ProductsListComponent,
+    },
 ];
 
 @NgModule({
